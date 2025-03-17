d53156a50ea9a44fd326941b26739c1b1e3ada3d
lguartazaca <lguartazaca@opcionauto.com>
2025-03-17 18:43:44 -0500

    feat(seccion2): fin next link

--- first-steps/components/navbar/NavBar.tsx
+++ first-steps/components/navbar/NavBar.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import Link from "next/link";
+import {HomeIcon} from "@primer/octicons-react";
 
-export  const NavBar = () => {
+const navItems = [
+    {path: '/about', label: 'About'},
+    {path: '/pricing', label: 'Prices'},
+    {path: '/contact', label: 'Contact'},
+]
+export  const NavBar = async () => {
     console.log("NavBar creado 123");
     return (
         <nav className={'flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'}>
-            <span className={'font-bold text-xl'}>Home</span>
+           <Link href={'/'} className={'flex items-center'}>
+               <HomeIcon className={'mr-2'} />
+               <span className={'font-bold text-xl'}>Home</span>
+           </Link>
             <div className={'flex flex-1'}></div>
-            <a className={'mr-2 text-lg'} href="/about">About</a>
-            <a className={'mr-2 text-lg'} href="/pricing">Pricing</a>
-            <a className={'mr-2 text-lg'} href="/contact">Contact</a>
+            {
+                navItems.map(items => (
+                    <Link key={items.path} href={items.path} className={'mr-2 text-lg'}>{items.label}</Link>
+                ))
+            }
         </nav>
     );
 };
